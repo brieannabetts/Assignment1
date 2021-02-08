28fe64365f20220a6163ab5015b22843e206fa45
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 12:53:08 -0600

    connect check

--- task-list.php
+++ task-list.php
@@ -5,7 +5,15 @@
         <title>Task List</title>
     </head>
     <body>
-<?php
-?>
+    <?php
+    $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
+    if (!$db)  {
+        echo 'could not connect';
+    }
+    else {
+        echo 'connected to the database';
+    }
+    $db = null;
+    ?>
     </body>
 </html>
