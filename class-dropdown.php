14db5e75fdca231171f2a404ca171ec0c5e0f6ec
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 16:07:24 -0600

    Comments

--- class-dropdown.php
+++ class-dropdown.php
@@ -6,11 +6,14 @@
 </head>
 <body>
 <h1>Type in Your Class</h1>
+// move to save-class.php page
 <form method="post" action="save-class.php">
     <fieldset>
+        //label and input for class
         <label for="class">Class Name: </label>
         <input name="class" id="class" required />
     </fieldset>
+    // save button
     <button>Save</button>
 </form>
 </body>
