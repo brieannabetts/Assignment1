3cc7f7b21c1c661f48e540a9bb90a77626a1c4e3
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 16:06:50 -0600

    Comments

--- tasks.php
+++ tasks.php
@@ -6,32 +6,38 @@
     </head>
     <body>
         <h1>Tasks</h1>
+        //move to save-task php page
         <form method="post" action="save-task.php">
             <fieldset>
+                //label and input for task
                 <label for="task">Task: </label>
                 <input name="task" id="task" required />
             </fieldset>
             <fieldset>
+                //label and input for duedate
                 <label for="duedate">Due Date: </label>
                 <input name="duedate" id="duedate">
             </fieldset>
             <fieldset>
+                //label and dropdown selection for class
                 <label for="class">Class: </label>
                 <select name="class" id="class" required />
                 <?php
+                //connect
                 $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
-
-                $sql = "SELECT * FROM dropdown";
-
+                //select data
+                $sql = "SELECT * FROM class";
+                // store and collect data
                 $cmd = $db->prepare($sql);
                 $cmd->execute();
                 $class = $cmd->fetchAll();
-
+                //make a drop down option
                 foreach ($class as $c) {
                     echo '<option value="' . $c['class'].'"</option>';
                 }
                 ?>
              </fieldset>
+            //save button
             <button>Save</button>
         </form>
     </body>
