3aa0b73ef7cf805f60e11f0c5165c9e6317ca1bb
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 13:49:08 -0600

    dropdown

---
+++ class-dropdown.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Class</title>
+</head>
+<body>
+<h1>Type in Your Class</h1>
+<form method="post" action="save-class.php">
+    <fieldset>
+        <label for="class">Class Name: </label>
+        <input name="class" id="class" required />
+    </fieldset>
+    <button>Save</button>
+</form>
+</body>
+</html>
