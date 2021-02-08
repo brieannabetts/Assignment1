058042b38336f79964910853cb9c2d8ee84e00b5
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 15:46:17 -0600

    task list complete

--- task-list.php
+++ task-list.php
@@ -6,13 +6,36 @@
     </head>
     <body>
     <?php
-    $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
-    if (!$db)  {
-        echo 'could not connect';
+    // 1. Connect to the db.  Host: 172.31.22.43, DB: dbNameHere, Username: usernameHere, PW: passwordHere
+    $user = 'Brieanna1158732';
+    $database = 'Brieanna1158732';
+    $passw = 'xz0PV1TNW9O';
+
+    try {
+        $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
+    } catch (PDOException $e) {
+        echo "Error when connecting to database: " . $e->getMessage();
+        die();
     }
-    else {
-        echo 'connected to the database';
+
+    $sql = "SELECT * FROM tasks";
+
+    $cmd = $db->prepare($sql);
+    $cmd->execute();
+
+    $tasks = $cmd->fetchAll();
+
+    echo '<table class= "table-striped table-primary"><thead><th>Task</th><th>Class</th><th>Due Date</th></thead>';
+
+    foreach ($tasks as $indTasks)
+    {
+        echo '<tr><td>' . $indTasks['task'] . '</td>
+        <td>' . $indTasks['class'] . '</td>
+        <td>'. $indTasks['duedate'].'</td></tr>';
     }
+
+    echo '</table>';
+
     $db = null;
     ?>
     </body>
