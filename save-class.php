75bb783202192063759056e6ce609da06449f4bf
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 14:12:52 -0600

    save-class complete

--- save-class.php
+++ save-class.php
@@ -6,6 +6,29 @@
 </head>
 <body>
 <?php
+$class = $_POST['class'];
+$ok = true;
+
+if (empty($class)) {
+    echo 'Class is required';
+    $ok = false;
+}
+
+if ($ok) {
+    $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
+
+    $sql = "INSERT INTO dropdown (class) VALUES (:class)";
+
+    $cmd = $db->prepare($sql);
+    $cmd->bindParam(':class', $class, PDO::PARAM_STR, 100);
+
+    $cmd->execute();
+
+    $db = null;
+
+    echo "<h1>Class Saved</h1>";
+}
+
 ?>
 </body>
 </html>
