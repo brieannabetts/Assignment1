ce6dce5dba257e85c80c258a20e9b26876bc4254
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 16:17:25 -0600

    Comments

--- save-task.php
+++ save-task.php
@@ -6,47 +6,53 @@
 </head>
 <body>
 <?php
-// 1. store the form inputs in variables (optional but reduces syntax errors)
+// store variables
 $task = $_POST['task'];
 $duedate = $_POST['duedate'];
 $class= $_POST['class'];
+// make a flag
 $ok = true;
 
-// 1a. validate inputs before saving
+// make sure everything has been filled in
+//if the  task is empty
 if (empty(trim($task))) {
+    //let the user know the task is required
     echo 'Task is required<br />';
+    // set flag to false
     $ok = false;
 }
-
+// if the due date is empty
 if (empty($duedate)) {
+    // let user know due date is required
     echo 'Due Date is required<br />';
+    // set flag to false
     $ok = false;
 }
-
+// if the class is empty
 if (empty($class)) {
+    //let user know class is required
     echo 'Class is required<br />';
+    // set flag to false
     $ok = false;
 }
-
+// if flaq is true
 if ($ok) {
+    // connect
     $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
 
-
+    // insert and merge into one table
     $sql = "INSERT INTO task (task, duedate, class) VALUES (:task, :duedate, :class)";
-
-    // 4. populate the INSERT with our variables using a Command variable to prevent SQL Injection
     $cmd = $db->prepare($sql);
     $cmd->bindParam(':task', $task, PDO::PARAM_STR, 40);
     $cmd->bindParam(':duedate', $duedate, PDO::PARAM_STR, 8);
     $cmd->bindParam(':class', $class, PDO::PARAM_STR, 40);
 
-    // 5. execute the INSERT to save the data
+    //execute
     $cmd->execute();
 
-    // 6. disconnect
+    // disconnect
     $db = null;
 
-    // 7. show confirmation message to user
     echo "<h1>Item Saved</h1>";
 }
 ?>
