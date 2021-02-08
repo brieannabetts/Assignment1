058d4b4203e26705ad2176b72f1fc198a8d2ae45
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 12:38:15 -0600

    Label and Input set up tasks file complete

--- tasks.php
+++ tasks.php
@@ -5,5 +5,20 @@
         <title> Tasks </title>
     </head>
     <body>
+    <h1>Tasks</h1>
+    <form method = "post" action="task-list.php">
+        <fieldset>
+            <label for = "task" > Task: </label>
+            <input name ="task" id ="task" required>
+        </fieldset>
+        <fieldset>
+            <label for = "class"> Class: </label>
+            <input name="class" id ="class" required>
+        </fieldset>
+        <fieldset>
+            <label for = "duedate"> Due Date: </label>
+            <input name = "duedate" id = "duedate" required>
+        </fieldset>
+        <button>Save</button>
     </body>
 </html>
