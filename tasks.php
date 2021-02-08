207fcb70d2855ae7b28bba25d085475f0d7e9fc9
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-08 14:10:10 -0600

    tasks complete

--- tasks.php
+++ tasks.php
@@ -1,24 +1,38 @@
 <!DOCTYPE html>
-<html lang = "en">
+<html lang="en">
     <head>
-        <meta charset = "UTF-8">
-        <title> Tasks </title>
+        <meta charset="UTF-8">
+        <title>Tasks</title>
     </head>
     <body>
-    <h1>Tasks</h1>
-    <form method = "post" action="task-list.php">
-        <fieldset>
-            <label for = "task" > Task: </label>
-            <input name ="task" id ="task" required>
-        </fieldset>
-        <fieldset>
-            <label for = "class"> Class: </label>
-            <input name="class" id ="class" required>
-        </fieldset>
-        <fieldset>
-            <label for = "duedate"> Due Date: </label>
-            <input name = "duedate" id = "duedate" required>
-        </fieldset>
-        <button>Save</button>
+        <h1>Tasks</h1>
+        <form method="post" action="save-task.php">
+            <fieldset>
+                <label for="task">Task: </label>
+                <input name="task" id="task" required />
+            </fieldset>
+            <fieldset>
+                <label for="duedate">Due Date: </label>
+                <input name="duedate" id="duedate">
+            </fieldset>
+            <fieldset>
+                <label for="class">Class: </label>
+                <select name="class" id="class" required />
+                <?php
+                $db = new PDO('mysql:host=172.31.22.43;dbname=Brieanna1158732', 'Brieanna1158732', 'z0PV1TNW9O');
+
+                $sql = "SELECT * FROM dropdown";
+
+                $cmd = $db->prepare($sql);
+                $cmd->execute();
+                $dropdown = $cmd->fetchAll();
+
+                foreach ($dropdown as $c) {
+                    echo '<option value="' . $c['class'].'"</option>';
+                }
+                ?>
+             </fieldset>
+            <button>Save</button>
+        </form>
     </body>
 </html>
