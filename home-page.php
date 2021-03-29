2124b56c281efcb626678c9e72f89303bc044fac
brieannabetts <bgbetts@lakeheadu.ca>
2021-03-29 08:39:11 -0500

    Home Page Complete

---
+++ home-page.php
@@ -0,0 +1,10 @@
+<?php $pageTitle = 'Welcome!';
+include 'header.php'; ?>
+
+<main class="container">
+    <h1>SCHOOL SCHEDULE</h1>
+    <p>This site is for you to create a to-do list for all the assignments/labs/quizzes
+        etc so you can stay organized in school.</p>
+    <p>The application uses PHP, MySQL, and PDO and is hosted on AWS.</p>
+    <p>Source Code available on <a href="https://github.com/brieannabetts/Comp1006-Assignment">GitHub</a>.</p>
+</main>
